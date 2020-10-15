b0750674e26fabc3ef917443b3a8fce19ae8320a
Ricardo Gonçalves Ferreira <rgf.ricardo@gmail.com>
2020-10-14 23:40:54 -0300

    Adicionado configuracao do postgres no arquivo de configuracao

--- app/config.py
+++ app/config.py
@@ -6,10 +6,11 @@ GITHUB_API_URL = "https://api.github.com"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 POSTGRES_USER = os.environ.get("POSTGRES_USER")
 POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+POSTGRES_ADDRESS = os.environ.get("POSTGRES_ADDRESS", 'localhost')
 
-if POSTGRES_USER and POSTGRES_PASSWORD:
-    SQLALCHEMY_DATABASE_URI = "postgresql://{}:{}@postgres:5432/github_api".format(
-        POSTGRES_USER, POSTGRES_PASSWORD
+if POSTGRES_USER and POSTGRES_PASSWORD and POSTGRES_ADDRESS:
+    SQLALCHEMY_DATABASE_URI = "postgresql://{}:{}@{}:5432/github_api".format(
+        POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_ADDRESS
     )
 else:
     SQLALCHEMY_DATABASE_URI = "sqlite:////tmp/test.db"
