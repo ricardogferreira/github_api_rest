49bc3673b8e59926175f5a8f7c92e3a97d4998e6
Ricardo Gonçalves Ferreira <rgf.ricardo@gmail.com>
2020-10-15 23:22:01 -0300

    Adicionado mais fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,13 +7,18 @@ def username():
 
 
 @fixture
-def owner(username):
-    return {"id": 123, "login": username}
+def user_id():
+    return 123
+
+
+@fixture
+def owner(username, user_id):
+    return {"id": user_id, "login": username}
 
 
 @fixture
 def repository_name():
-    return "test_repository_1222"
+    return "test_repository1"
 
 
 @fixture
@@ -26,10 +31,39 @@ def repository(owner, repository_name):
 
 
 @fixture
-def user(username):
-    {"user_id": 123, "username": username}
+def repository2(owner):
+    return {"name": "repo2", "size": 3, "owner": owner}
 
 
 @fixture
-def parsed_repository():
-    return {"name": "test_repository_1222", "size": 150}
+def user(username, user_id):
+    {"user_id": user_id, "username": username}
+
+
+@fixture
+def parsed_repository(repository_name):
+    return {"name": repository_name, "size": 150}
+
+
+@fixture
+def parsed_repository2():
+    return ({"name": "repo2", "size": 3, "owner": owner},)
+
+
+@fixture
+def repositories(repository, repository2):
+    return [repository, repository2]
+
+
+@fixture
+def parsed_repositories(parsed_repository, parsed_repository2):
+    return [parsed_repository, parsed_repository2]
+
+
+@fixture
+def user_repositories(username, user_id):
+    return {
+        "username": username,
+        "user_id": user_id,
+        "repositories": parsed_repositories,
+    }
